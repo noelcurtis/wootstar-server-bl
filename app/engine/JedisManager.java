e9bb9d2be61d1ce2035a5c9c618f0fc070cd3053
Noel Curtis <noelcurtis@gmail.com>
2013-12-02 22:55:53 -0500

    Updated jedis config.

--- app/engine/JedisManager.java
+++ app/engine/JedisManager.java
@@ -24,18 +24,10 @@ public class JedisManager
     {
         String host = Play.application().configuration().getString("redis.host");
         String port = Play.application().configuration().getString("redis.port");
-        String password = Play.application().configuration().getString("redis.password");
-        if (Strings.isNullOrEmpty(password))
-        {
-            pool = new JedisPool(new JedisPoolConfig(), host);
-        }
-        else
-        {
-            JedisPoolConfig pc = new JedisPoolConfig();
-            pc.setMaxActive(3);
-            pc.setMaxIdle(2);
-            pool = new JedisPool(pc, host, Utils.toInt(port, "0"), 20000, password);
-        }
+        JedisPoolConfig pc = new JedisPoolConfig();
+        //pc.setMaxActive(3);
+        //pc.setMaxIdle(2);
+        pool = new JedisPool(pc, host, Utils.toInt(port, "0"), 20000);
     }
 
     public JedisPool getPool()
