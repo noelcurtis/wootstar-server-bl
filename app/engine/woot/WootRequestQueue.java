8603df4c213eca51728315fea9b5049f3a9de3f7
Noel Curtis <noelcurtis@gmail.com>
2014-02-23 14:44:31 -0500

    removing flush cache.

--- app/engine/woot/WootRequestQueue.java
+++ app/engine/woot/WootRequestQueue.java
@@ -51,7 +51,7 @@ public class WootRequestQueue
 
     public void scheduleRequests()
     {
-        JedisManager.SharedJedisManager().flush();
+//        JedisManager.SharedJedisManager().flush();
         int t = 0;
         for (final WootRequest r : requests)
         {
